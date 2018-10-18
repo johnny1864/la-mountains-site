03d4c01c235cd6333eb267449d9f3c05a2e08d21
Johnny <johnny1864@yahoo.com>
2018-10-17 18:47:04 -0700

    first commit

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+"use strict";
+
+window.addEventListener('load', function () {
+
+    const header = document.querySelector('header');
+
+
+    window.addEventListener('scroll', function () {
+        let distanceScrolled = window.scrollY;
+
+        // console.log('test', distanceScrolled);
+        if (distanceScrolled > 75) {
+            header.classList.add('scroll-nav');
+        }else {
+            header.classList.remove('scroll-nav');
+        }
+    })
+
+}); // END OF LOAD FUNCTION
